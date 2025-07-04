9e33dd49488892e11bbd0a190fe0e48581b1014d
RAKESH x OFFICIAL <154747451+Rakeshxofficial@users.noreply.github.com>
2025-07-04 14:44:56 +0530

    Updated netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,20 +18,5 @@ export default defineConfig({
       'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY || ''),
     },
     envPrefix: 'VITE_',
-    build: {
-      cssCodeSplit: true,
-      minify: true,
-      assetsInlineLimit: 4096, // 4kb
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'vendor': ['@supabase/supabase-js'],
-          }
-        }
-      }
-    },
-    ssr: {
-      noExternal: ['@supabase/supabase-js']
-    }
   },
 });
