c6e4c678ebbec0c3650d94f6cbe3aff9fdb3d88f
RAKESH x OFFICIAL <154747451+Rakeshxofficial@users.noreply.github.com>
2025-07-07 14:35:14 +0530

    Updated package-lock.json

--- scripts/indexnow.js
+++ scripts/indexnow.js
@@ -4,7 +4,8 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname } from 'path'; 
+import fetch from 'node-fetch';
 
 // Get the directory name
 const __filename = fileURLToPath(import.meta.url);
