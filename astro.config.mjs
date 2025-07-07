3d80a9c9b6edff0a0903b40485baed52aef20f48
RAKESH x OFFICIAL <154747451+Rakeshxofficial@users.noreply.github.com>
2025-07-07 11:37:22 +0530

    Updated package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
-import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push', 'gtag'],
-      },
-    }),
-  ],
-  trailingSlash: 'ignore',
+  integrations: [tailwind()],
   output: 'server', // Change from 'hybrid' to 'server' for Netlify deployment
   adapter: netlify(),
   compressHTML: true,
