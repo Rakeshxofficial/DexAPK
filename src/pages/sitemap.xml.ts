f11bfcdc8620e6e1d8c9b66c7b4f7a1191cfc93b
RAKESH x OFFICIAL <154747451+Rakeshxofficial@users.noreply.github.com>
2025-07-13 21:21:14 +0530

    Fix sitemap.xml namespace errors

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -11,7 +11,9 @@ export async function GET() {
   
   // Generate sitemap XML
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <!-- Main Pages -->
   <url>
     <loc>${baseUrl}/</loc>
