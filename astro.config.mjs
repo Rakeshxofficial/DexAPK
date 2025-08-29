8e034ec3b0092224ee3202b6bd9e711e1978536b
Rakesh <154747451+Rakeshxofficial@users.noreply.github.com>
2025-08-30 00:34:28 +0800

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,10 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: 'static',
   adapter: netlify({
-    edgeMiddleware: true,
+    edgeMiddleware: false,
+    functionPerRoute: false,
     binaryMediaTypes: []
   }),
   compressHTML: true,
