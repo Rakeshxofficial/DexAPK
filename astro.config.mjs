fef2e4e17f7cac38d0f4f5cd45a97875f48bb41e
Rakesh <154747451+Rakeshxofficial@users.noreply.github.com>
2025-08-30 00:23:56 +0800

    Fix Astro configuration for version 5.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
+  output: 'static',
   adapter: netlify({
     edgeMiddleware: false,
     functionPerRoute: false,
